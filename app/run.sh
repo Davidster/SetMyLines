020bb36d712669b9c046440c8f3bf03134ab9129
David Huculak <david.huculak@classmethod.ca>
2019-02-17 15:53:57 -0500

    created systemd service so app runs in the background; added installation script for production

--- app/run.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-DEBUG=app:* CLIENT_ID=$(cat ../conf/clientID) CLIENT_SECRET=$(cat ../conf/clientSecret) npm start
---
+++ app/runDev.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+DEBUG=app:* CLIENT_ID=$(cat conf/clientID) CLIENT_SECRET=$(cat conf/clientSecret) npm start
