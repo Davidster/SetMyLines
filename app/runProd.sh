c15f549be9f03e5781a721ed31089c7af11e7790
David Huculak <david.huculak@classmethod.ca>
2019-02-17 16:46:22 -0500

    fix issue where iptable is not set on boot

--- app/runProd.sh
+++ app/runProd.sh
@@ -2,4 +2,4 @@
 
 cd /opt/yahoo-fantasy-automation/app
 DEBUG=app:* CLIENT_ID=$(cat conf/clientID) CLIENT_SECRET=$(cat conf/clientSecret) \
-/root/.nvm/versions/node/v10.15.1/bin/npm run start
+/root/.nvm/versions/node/v10.15.1/bin/node /opt/yahoo-fantasy-automation/app/bin/www
