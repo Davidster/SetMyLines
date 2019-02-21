9903d768c1c21398f60d430bc94aecaf63ed28b3
David Huculak <davidhuculak5@gmail.com>
2019-02-21 00:41:19 -0500

    added dotenv package

--- app/runProd.sh
+++ app/runProd.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 cd /opt/yahoo-fantasy-automation/app
-NODE_ENV=production DEBUG=app:* CLIENT_ID=$(cat conf/clientID) CLIENT_SECRET=$(cat conf/clientSecret) \
+NODE_ENV=production DEBUG=app:* \
 /root/.nvm/versions/node/v10.15.1/bin/node /opt/yahoo-fantasy-automation/app/bin/www
